3270d58d4a5bb2fe4bf175d00e5e8fb6637b2e9e
Andriy Kryvenko <krivenko.a.b@gmail.com>
2023-03-22 19:39:01 +0200

    Fixed admin breadcrumbs

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -268,7 +268,7 @@
                 <div class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
                     <div class="w-full mb-1">
                         <div class="mb-4">
-                            {{ Breadcrumbs::render('admin.catalog.products.create') }}
+                            {{ Breadcrumbs::render() }}
                             <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">All products</h1>
                         </div>
 
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -7,19 +7,33 @@ use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 Breadcrumbs::for('admin.dashboard', function (BreadcrumbTrail $trail) {
     $trail->push('Home', route('admin.dashboard'));
 });
-Breadcrumbs::for('admin.catalog', function (BreadcrumbTrail $trail) {
+Breadcrumbs::for('admin.catalog.index', function (BreadcrumbTrail $trail) {
     $trail->parent('admin.dashboard');
     $trail->push('Catalog', route('admin.catalog.index'));
 });
-Breadcrumbs::for('admin.catalog.products', function (BreadcrumbTrail $trail) {
-    $trail->parent('admin.catalog');
+
+Breadcrumbs::for('admin.catalog.products.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('admin.catalog.index');
     $trail->push('Products', route('admin.catalog.products.index'));
 });
 Breadcrumbs::for('admin.catalog.products.create', function (BreadcrumbTrail $trail) {
-    $trail->parent('admin.catalog.products');
+    $trail->parent('admin.catalog.products.index');
     $trail->push('Create product');
 });
 Breadcrumbs::for('admin.catalog.products.edit', function (BreadcrumbTrail $trail) {
-    $trail->parent('admin.catalog.products');
+    $trail->parent('admin.catalog.products.index');
     $trail->push('Edit product');
+});
+
+Breadcrumbs::for('admin.catalog.properties.index', function (BreadcrumbTrail $trail) {
+    $trail->parent('admin.catalog.index');
+    $trail->push('Properties', route('admin.catalog.properties.index'));
+});
+Breadcrumbs::for('admin.catalog.properties.create', function (BreadcrumbTrail $trail) {
+    $trail->parent('admin.catalog.properties.index');
+    $trail->push('Create property');
+});
+Breadcrumbs::for('admin.catalog.properties.edit', function (BreadcrumbTrail $trail) {
+    $trail->parent('admin.catalog.properties.index');
+    $trail->push('Edit property');
 });
